92f3a6dac0c003acb1ac03efb0f7c026af2462be
syj11545 <tjdbswo0401@naver.com>
2025-11-20 05:46:40 +0900

    syj

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const db = require('./config/database'); //서버가 켜질 때 DB 연결 시도
 const authRoutes = require('./routes/auth');
 const scheduleRoutes = require('./routes/schedule');
+const holidayRoutes = require('./routes/holiday');
 
 // 설정: 서버 포트 번호 (3000번)
 const PORT = 3000;
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 // 라우트 등록
 app.use('/api/auth', authRoutes);
 app.use('/api/schedules', scheduleRoutes);
+app.use('/api/holidays', holidayRoutes);
 
 // 기본 경로('/')로 접속했을 때 index.html 보내주기
 app.get('/', (req, res) => {
