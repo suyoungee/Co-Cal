bf2e833a82b9e56f0e4cae7fac1691aa69d3a9e5
geun95 <g95eun@gmail.com>
2025-11-21 00:56:49 +0900

    feat: 일정 조율 기능 추가

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const db = require('./config/database'); //서버가 켜질 때 DB 연결 시도
 const authRoutes = require('./routes/auth');
 const scheduleRoutes = require('./routes/schedule');
 const holidayRoutes = require('./routes/holiday');
+const timetableRoutes = require('./routes/timetable');
 
 // 설정: 서버 포트 번호 (3000번)
 const PORT = 3000;
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/schedules', scheduleRoutes);
 app.use('/api/holidays', holidayRoutes);
+app.use('/api/timetable', timetableRoutes);
 
 // 기본 경로('/')로 접속했을 때 index.html 보내주기
 app.get('/', (req, res) => {
